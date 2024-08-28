6a09364b2799eacc42dfc0303c403b5d4b3fe8aa
onderakbulut <onderakbulut@yahoo.com>
2024-08-28 12:45:56 +0300

    Add portfolio section to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 
 import Hero from '@/components/hero/hero';
+import Portfolio from '@/components/portfolio/portfolio';
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
         <Hero />
+        <Portfolio />
     </main>
   );
 }
